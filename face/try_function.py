7897d3f350ae5313d205e59e6cc732a28ec22e4d
JoannaMao222 <126645116+JoannaMao222@users.noreply.github.com>
2023-03-01 15:34:23 +0800

    Add files via upload

---
+++ face/try_function.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  1 13:39:07 2023
+
+@author: Joanna Mao
+"""
+
+def square(number):
+    return number ** 2
+
+print (square(7))
+print ("\n")
+print (square(4))
